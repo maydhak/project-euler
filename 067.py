cf91ef485d365f728912623baea405b6702a9b79
maydha <maydhak@gmail.com>
2020-01-05 22:24:33 -0600

    cleaned up problem 18, added problem 67

--- 067.py
+++ 067.py
@@ -1,6 +1,6 @@
 '''
-By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top
-to bottom is 23.
+By starting at the top of the triangle below and moving to adjacent numbers on the row below,
+the maximum total from top to bottom is 23.
 
    3
   7 4
@@ -9,46 +9,19 @@ to bottom is 23.
 
 That is, 3 + 7 + 4 + 9 = 23.
 
-Find the maximum total from top to bottom of the triangle below:
-
-                            75
-                          95 64
-                        17 47 82
-                      18 35 87 10
-                    20 04 82 47 65
-                  19 01 23 75 03 34
-                88 02 77 73 07 63 67
-              99 65 04 28 06 16 70 92
-            41 41 26 56 83 40 80 70 33
-          41 48 72 33 47 32 37 16 94 29
-        53 71 44 65 25 43 91 52 97 51 14
-      70 11 33 28 77 73 17 78 39 68 17 57
-    91 71 52 38 17 14 91 43 58 50 27 29 48
-  63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+Find the maximum total from top to bottom in triangle.txt, a 15K text file
+containing a triangle with one-hundred rows.
 '''
-
-big_triangle = '''75
-                         95 64
-                        17 47 82
-                      18 35 87 10
-                    20 04 82 47 65
-                  19 01 23 75 03 34
-                88 02 77 73 07 63 67
-              99 65 04 28 06 16 70 92
-            41 41 26 56 83 40 80 70 33
-          41 48 72 33 47 32 37 16 94 29
-        53 71 44 65 25 43 91 52 97 51 14
-      70 11 33 28 77 73 17 78 39 68 17 57
-    91 71 52 38 17 14 91 43 58 50 27 29 48
-  63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'''
 
 
 def parse_rows(triangle):
-    # Splits the triangle into its rows and creates a matrix (list of lists)
-    # Each list is a row, elements of the list are values in the row
+    # Splits the triangle into its rows
     rows = triangle.split("\n")
+    return rows
+
+
+def create_matrix(rows):
+    # Creates a matrix (list of lists) where each list is a row, elements of the list are values in the row
     matrix = []
     for row in rows:
         matrix.append(row.lstrip().split(" "))
@@ -94,9 +67,10 @@ def pick_path(cell, matrix):  # cell is a Cell object, matrix holds all Cell obj
         cell.max_side = 'r'
 
 
-def solution18(triangle):
+def solution67(triangle):
     # set up the matrix from the triangle
-    matrix = parse_rows(triangle)
+    rows = parse_rows(triangle)
+    matrix = create_matrix(rows)
     real_matrix = create_expanded_matrix(matrix)
 
     # find the best path - start with the second to last row and work backwards
@@ -105,4 +79,6 @@ def solution18(triangle):
             pick_path(cell, real_matrix)
     return real_matrix[0][0].max_sum
 
-print(solution18(big_triangle))
+
+big_triangle = open("067_triangle.txt").read()
+print(solution67(big_triangle))
